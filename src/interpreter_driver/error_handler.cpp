37f83587e24020ade3a945257b2673ae5a5e3aaa
dhmbb <yjqsjtu2022@sjtu.edu.cn>
2024-04-02 14:29:48 +0800

    add error handler for parser and little syn code

--- src/interpreter_driver/error_handler.cpp
+++ src/interpreter_driver/error_handler.cpp
@@ -3,19 +3,24 @@
 #include <format>
 #include "interpreter_driver/initializer.h"
 #include "interpreter_driver/error_handler.h"
+#include "token.h"
 
 namespace clox{
 
 extern bool had_error;
 
-void error(int line, const std::string& message) {
+void error(Token token, const std::string& message) {
     had_error = true;
-    const std::string where;
-    report(line, message, where);
+    int line = token._line;
+    std::string where = token._lexeme.value();
+    if (token._type == TokenType::MYEOF)
+        report(line, message, "at end");
+    else
+        report(line, message, where);
 }
 
 void report(int line, const std::string& message, const std::string& where) {
-    std::string error_string = std::format("Error: {} \n line {}: {}", message, line, where);
+    std::string error_string = std::format("Error: {} \n line {}: at {}", message, line, where);
     std::cerr << error_string;
 }
 
